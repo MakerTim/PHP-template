72c91bde32e1f8071ed7013b44111e1ba5c30606
Tim <makertim@outlook.com>
2015-03-18 16:31:17 +0100

    highlight menu

--- JavaScript/animations.js
+++ JavaScript/animations.js
@@ -3,15 +3,13 @@ $(function() {
 });
 var menu;
 function highlightMenu() {
-	if(PHP.page.hasSubMenu == "false"){
-		jQuery("#Menu_" + PHP.page.menuEntry)
-			.animate({
-				backgroundColor: "#ffffff",
-				opacity: 0.5 
-				}, 1500)
-			.animate({
-				borderRadius: 15,
-				borderWidth: 3
-				});
-	}
+	jQuery("#Menu_" + PHP.page.menuEntry)
+		.animate({
+			backgroundColor: "#ffffff",
+			opacity: 0.5 
+			}, 1500)
+		.animate({
+			borderRadius: 15,
+			borderWidth: 3
+			});
 }
