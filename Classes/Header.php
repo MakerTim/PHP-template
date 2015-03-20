1af33a908cd120dc4c825bca84670443203177c4
Tim <makertim@outlook.com>
2015-03-20 09:32:11 +0100

    Time for the mobiel
    cleand up css and started on mobiel

--- Classes/Header.php
+++ Classes/Header.php
@@ -8,8 +8,10 @@
 		'<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.css" />',
 		'<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>',
 		'<link href="%root%/CSS/style.css" type="text/css" rel="stylesheet" />',
+		'<link href="%root%/CSS/mobiel.css" type="text/css" rel="stylesheet" />',
 		'<link href="%root%/CSS/markdown.css" type="text/css" rel="stylesheet" />',
 		'<link href="%root%/CSS/SubMenuStyle.css" type="text/css" rel="stylesheet" />',
+		'<link href="%root%/CSS/SubMenuStyleMobiel.css" type="text/css" rel="stylesheet" />',
 		'<script src="%root%/JavaScript/looks.js"></script>');
 		
 		public static function getMetaTags() {
--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	</head>
 
 	<body>
-		<div class="Pagina">
+		<div id="Pagina" class="Pagina">
 			<!-- Banner -->
 			<?php
 				if($pageContent->hasBanner == "true") {
