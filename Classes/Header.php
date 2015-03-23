f01f0ffcbe85835f6b9d6be0657ef8e04a35dcb7
Tim <makertim@outlook.com>
2015-03-23 10:32:53 +0100

    Disign it the limit
    Changed some stuff in the design to make it waaaay more nice

--- Classes/Header.php
+++ Classes/Header.php
@@ -7,6 +7,7 @@
 		'<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>', 
 		'<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.css" />',
 		'<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>',
+		'<link href="http://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" type="text/css" />',
 		'<link href="%root%/CSS/style.css" type="text/css" rel="stylesheet" />',
 		'<link href="%root%/CSS/mobiel.css" type="text/css" rel="stylesheet" />',
 		'<link href="%root%/CSS/markdown.css" type="text/css" rel="stylesheet" />',
