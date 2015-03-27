bb0f22f2fb72e2172c8fba9091bfa864532a4933
Tim <makertim@outlook.com>
2015-03-27 16:51:35 +0100

    My Error will be hidden

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	//error_reporting(E_ERROR & E_PARSE);
+	error_reporting(E_ERROR & E_PARSE);
 	require(join(DIRECTORY_SEPARATOR, array('.', 'Classes', 'IncludeManager.php')));
 	$pageContent = MktPage::getPage();
 	Counter::Count($pageContent);
