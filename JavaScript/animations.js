ef49ff283c724aa8b7d4fe373fe7aa06cb0ef36c
tim <makertim@outlook.com>
2015-04-20 16:49:17 +0200

    Error - Menu niet selected?

--- JavaScript/animations.js
+++ JavaScript/animations.js
@@ -9,15 +9,17 @@ function highlightMenu() {
 	} else {
 		$(".menuSelected").css("display", "inline");
 	}
-	color = $(".menuSelected").css("border").split("(")[1].split(")")[0].split(", ");
-	$(".menuSelected")
-		.animate({
-			backgroundColor: "rgba(" + color[0] + "," + color[1] + "," + color[2] + ",0.5)",
-			borderBottomRightRadius: 15,
-			borderBottomLeftRadius: 15,
-			paddingBottom: 25,
-			paddingLeft: 14,
-			paddingRight: 14,
-			borderWidth: 2
-			}, 500);
+	if($(".menuSelected").size() > 0){
+		color = $(".menuSelected").css("border").split("(")[1].split(")")[0].split(", ");
+		$(".menuSelected")
+			.animate({
+				backgroundColor: "rgba(" + color[0] + "," + color[1] + "," + color[2] + ",0.5)",
+				borderBottomRightRadius: 15,
+				borderBottomLeftRadius: 15,
+				paddingBottom: 25,
+				paddingLeft: 14,
+				paddingRight: 14,
+				borderWidth: 2
+				}, 500);
+	}
 }
