c5346a239c993a3acebde42908d0c886f2fcc9e2
tim <makertim@outlook.com>
2015-06-12 17:32:26 +0200

    external amps

--- external.php
+++ external.php
@@ -1,5 +1,6 @@
 <?php
 	if(isset($_GET) && isset($_GET["p"])){
-		echo file_get_contents("http://$_GET[p]");
+		$site = str_replace("$", "&", $_GET["p"]);
+		echo file_get_contents("http://$site");
 	}
 ?>
